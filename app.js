6f855ca273620b49d4f5de8378dd99ffd667988e
Marianaatecua17 <123569304+Marianaatecua17@users.noreply.github.com>
2023-02-08 10:14:04 -0500

    8/02/2023

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 document.addEventListener("DOMContentLoaded" , () => {
  const cardsAdj = [
    {
-     name: 'nacional escudo rosado'
-     img: 'img/nacionalrosado.png'
+     name: 'nacional1'
+     img: 'imges/nacional1.png'
    }
+   {
+    name: 'nacional2'
+    img: 'imges/nacional2.png'
+   }
+   {
+    name: 'nacional3'
+    img: 'imges/nacional3.png'
+   }
+   {
+    name: 'nacional4'
+    img: 'imges/nacional4.png'
+   }
+   {
+    name: 'nacional5'
+    img: 'imges/nacional5.png'
+   }
+   {
+    name: 'nacional6'
+    img: 'imges/nacional6.png'
+   }
+   {
+    name: 'nacional1'
+    img: 'imges/nacional1.png'
+  }
+  {
+   name: 'nacional2'
+   img: 'imges/nacional2.png'
+  }
+  {
+   name: 'nacional3'
+   img: 'imges/nacional3.png'
+  }
+  {
+   name: 'nacional4'
+   img: 'imges/nacional4.png'
+  }
+  {
+   name: 'nacional5'
+   img: 'imges/nacional5.png'
+  }
+  {
+   name: 'nacional6'
+   img: 'imges/nacional6.png'
+  }
+   
  ]
 });
